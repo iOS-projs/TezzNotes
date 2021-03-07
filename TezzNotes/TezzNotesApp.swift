d35dea4e97b0c8c004044cbc2b6a8941534974b6
Tejas M R <matblockgreninja@gmail.com>
2021-03-07 14:48:47 +0530

    Initial Commit

---
+++ TezzNotes/TezzNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TezzNotesApp.swift
+//  TezzNotes
+//
+//  Created by tezz on 07/03/21.
+//
+
+import SwiftUI
+
+@main
+struct TezzNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
